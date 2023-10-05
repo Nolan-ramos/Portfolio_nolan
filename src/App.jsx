88878ad4ff6b3407e97d275b9bfd4426052b9ed5
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-10-05 15:13:49 +0200

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
 import Cursor from './components/cursor/Cursor';
 import Navbar from './components/navbar/Navbar';
-import './reset.scss';
 import routes from './routes';
 
 function App() {
