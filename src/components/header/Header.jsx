191c8b2f38405d0f522d7d27ebeb1809149b0a02
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-10-07 13:32:46 +0200

    push

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
+import Close from '../../assets/close.svg';
+import Expand from '../../assets/expand.svg';
+import Subtract from '../../assets/subtract.svg';
 import './header.scss';
 
 function Header() {
     return (
         <div className='header'>
             <div className='header_fake_button_left'>
-                <span></span>
-                <span></span>
-                <span></span>
+                <span><img src={Close}/></span>
+                <span><img src={Subtract}/></span>
+                <span><img src={Expand}/></span>
             </div>
             <span className='header_title'>nolan_ramos.app</span>
             <div className='header_fake_button_right'></div>
--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -2,57 +2,51 @@ import React, { useEffect, useState } from 'react';
 import './title.scss';
 
 function Title(props) {
-    const [animatedTitle, setAnimatedTitle] = useState(props.title);
-    const animationDuration = 2000; 
-    const delayBetweenAnimations = 1000;
+    const [text, setText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [speed, setSpeed] = useState(300); // Vitesse de suppression/réécriture
 
     useEffect(() => {
-        const titleLength = props.title.length;
+        const titleText = props.title;
+        let timer;
 
-        let animationInterval;
-
-        const animateTitle = () => {
-            let i = 0;
-
-            animationInterval = setInterval(() => {
-                if (i < titleLength) {
-                    setAnimatedTitle(props.title.slice(0, titleLength - i));
-                    i++;
+        if (!isDeleting) {
+            timer = setTimeout(() => {
+                if (currentIndex < titleText.length) {
+                setText(titleText.slice(0, currentIndex + 1));
+                setCurrentIndex(currentIndex + 1);
                 } else {
-                    clearInterval(animationInterval);
-                    setTimeout(resetTitle, delayBetweenAnimations);
+                setIsDeleting(true);
+                setSpeed(300); // Vitesse de suppression
+                setCurrentIndex(titleText.length);
                 }
-            }, animationDuration / titleLength);
-        };
-
-        const resetTitle = () => {
-            let i = 0;
-            animationInterval = setInterval(() => {
-                if (i < titleLength) {
-                    setAnimatedTitle(props.title.slice(0, i + 1));
-                    i++;
+            }, speed);
+        } else {
+            timer = setTimeout(() => {
+                if (currentIndex > 0) {
+                setText(titleText.slice(0, currentIndex - 1));
+                setCurrentIndex(currentIndex - 1);
                 } else {
-                    clearInterval(animationInterval);
-                    setTimeout(animateTitle, delayBetweenAnimations);
+                setIsDeleting(false);
+                setSpeed(300); // Vitesse de réécriture
                 }
-            }, animationDuration / titleLength);
-        };
-
-        animateTitle();
+            }, speed);
+        }
 
         return () => {
-            clearInterval(animationInterval);
+            clearTimeout(timer);
         };
-    }, [props.title]);
+    }, [props.title, isDeleting, currentIndex, speed]);
 
     return (
         <h1>
             <span>{"{--"}</span>
-            {animatedTitle}
+            {text}
             <p>|</p>
             <span>{"--}"}</span>
         </h1>
-    )
+    );
 }
 
 export default Title;
