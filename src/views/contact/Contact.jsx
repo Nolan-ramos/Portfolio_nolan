8fec7c4f824b9b837d05f1c2ce6d44652b60d139
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-10-17 12:39:39 +0200

    contact

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -6,7 +6,20 @@ function Contact() {
     return (
         <div className='contact fade-in'>
             <Title title="Contact" />
-
+            <form name="contact" method="POST" netlify>
+                <p>
+                    <label>Votre nom : <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Votre email : <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Votre message : <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Envoyer</button>
+                </p>
+            </form>
             <Cursor />
         </div>
     )
