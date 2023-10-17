dac9616eb36fdc1fe7396fc3bda80c158b755b78
Nolan-ramos <nolan.ramos.01@gmail.com>
2023-10-17 18:33:39 +0200

    push

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -14,18 +14,30 @@ function Contact() {
                 data-netlify="true"
                 onSubmit="submit">
                 <input type="hidden" name="form-name" value="contact" />
-                <p>
-                    <label>Your Name: <input type="text" name="name"/></label>
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email"/></label>
-                </p>
-                <p>
+                    <label>Your Name: 
+                    <input type="text" name="name"/></label>
+                    <label>Your Email: 
+                    <input type="email" name="email"/></label>
                     <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
+                    <label>
+                    <input type="checkbox" name="option1" value="sujet 1" /> sujet 1
+                    </label>
+                    <label>
+                        <input type="checkbox" name="sujet" value="sujet 2" /> sujet 2
+                    </label>
+                    <label>
+                        <input type="checkbox" name="sujet" value="sujet 3" /> sujet 3
+                    </label>
+                    <label>
+                        <input type="checkbox" name="sujet" value="sujet 4" /> sujet 4
+                    </label>
+                    <label>
+                        <input type="checkbox" name="sujet" value="sujet 5" /> sujet 5
+                    </label>
+                    <label>
+                        <input type="checkbox" name="sujet" value="sujet 6" /> sujet 6
+                    </label>
                     <button type="submit">Envoyer</button>
-                </p>
             </form>
             <Cursor />
         </div>
