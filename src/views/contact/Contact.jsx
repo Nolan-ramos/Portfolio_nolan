d7e67aeb78d9ce603956577ee6da57a00a2c6272
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-10-17 13:42:52 +0200

    test

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,23 +1,11 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+// import axios from 'axios';
+// import { useState } from 'react';
 import Cursor from '../../components/cursor/Cursor';
 
 function Contact() {
-    const [message, setMessage] = useState('');
-
-    const sendMessage = async () => {
-        try {
-            const response = await axios.post('/api/message', { message: 'Hello from the client!' });
-            setMessage(response.data);
-        } catch (error) {
-            console.error('Error sending message:', error);
-        }
-    };
-
     return (
         <div className='contact fade-in'>
-            <button onClick={sendMessage}>Send Message</button>
-            <div>{message}</div>
+            <Title title="Contact" />
             <Cursor />
         </div>
     );
