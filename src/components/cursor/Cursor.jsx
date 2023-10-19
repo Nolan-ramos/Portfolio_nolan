05dcb55eab511a40aed1e799b760b1a0c4e2baab
Nolan-ramos <nolan.ramos.01@gmail.com>
2023-10-19 15:31:53 +0200

    contact and responsive all

--- src/components/cursor/Cursor.jsx
+++ src/components/cursor/Cursor.jsx
@@ -10,57 +10,36 @@ function Cursor() {
         setMousePosition({ x: e.clientX, y: e.clientY });
     };
 
-    const handleMouseEnterLink = () => {
+    const handleMouseEnter = () => {
         setCursorSize({ width: '20px', height: '20px' });
         setCursorBackgroundColor('rgba(0, 0, 0, 0)');
     };
-
-    const handleMouseLeaveLink = () => {
+    
+    const handleMouseLeave = () => {
         setCursorSize({ width: '10px', height: '10px' });
         setCursorBackgroundColor(getComputedStyle(document.documentElement).getPropertyValue('--primary-color'));
     };
-
-    const handleMouseEnterButton = () => {
-        setCursorSize({ width: '20px', height: '20px' });
-        setCursorBackgroundColor('rgba(0, 0, 0, 0)');
-    };
-
-    const handleMouseLeaveButton = () => {
-        setCursorSize({ width: '10px', height: '10px' });
-        setCursorBackgroundColor(getComputedStyle(document.documentElement).getPropertyValue('--primary-color'));
-    };
+    
+    useEffect(() => {
+        const elements = document.querySelectorAll('a, button, input, textarea, label');
+        elements.forEach((element) => {
+            element.addEventListener('mouseenter', handleMouseEnter);
+            element.addEventListener('mouseleave', handleMouseLeave);
+        });
+    
+        return () => {
+            elements.forEach((element) => {
+                element.removeEventListener('mouseenter', handleMouseEnter);
+                element.removeEventListener('mouseleave', handleMouseLeave);
+            });
+        };
+    }, []);
 
     useEffect(() => {
         document.addEventListener('mousemove', updateMousePosition);
 
         return () => {
             document.removeEventListener('mousemove', updateMousePosition);
-        };
-    }, []);
-
-    useEffect(() => {
-        const links = document.querySelectorAll('a');
-        links.forEach((link) => {
-            link.addEventListener('mouseenter', handleMouseEnterLink);
-            link.addEventListener('mouseleave', handleMouseLeaveLink);
-        });
-
-        const buttons = document.querySelectorAll('button');
-        buttons.forEach((button) => {
-            button.addEventListener('mouseenter', handleMouseEnterButton);
-            button.addEventListener('mouseleave', handleMouseLeaveButton);
-        });
-
-        return () => {
-            links.forEach((link) => {
-                link.removeEventListener('mouseenter', handleMouseEnterLink);
-                link.removeEventListener('mouseleave', handleMouseLeaveLink);
-            });
-
-            buttons.forEach((button) => {
-                button.removeEventListener('mouseenter', handleMouseEnterButton);
-                button.removeEventListener('mouseleave', handleMouseLeaveButton);
-            });
         };
     }, []);
 
