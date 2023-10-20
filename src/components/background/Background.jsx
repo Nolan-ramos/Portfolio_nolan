0945b60fa9479eca017a31ca65b3b20f9439edef
Nolan-ramos <nolan.ramos.01@gmail.com>
2023-10-20 10:40:43 +0200

    push

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
     return (
         <div className='background'>
             <div className='background_color'></div>
-            <div className='background_filtre' style={{ backdropFilter: 'blur(80px)' }}></div> {/* Ajout du backdrop-filter */}
+            <div className='background_filtre'></div>
             <div className='background_shapes'>
                 <div className='shape'></div>
                 <div className='shape'></div>
