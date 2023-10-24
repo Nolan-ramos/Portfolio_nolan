91c0972c6b951bfdce2547fb22157df520d016e5
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-10-24 15:21:24 +0200

    push all

--- src/views/portfolio/Portfolio.jsx
+++ src/views/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Cursor from '../../components/cursor/Cursor';
+import MacButtons from '../../components/mac_buttons/mac_buttons';
 import Title from '../../components/title/Title';
 import './portfolio.scss';
 import portfolioData from './portfolioData.json';
@@ -20,38 +21,50 @@ function Portfolio() {
     return (
         <div className='portfolio fade-in'>
         <Title title="Portfolio" />
-
-        <div className="language-container">
-            {Object.keys(portfolioData.languages).map((language) => (
-            <div key={language} onClick={() => handleLanguageClick(language)}>
-                {language}
+        <div className='portfolio_container'>
+            <div className='portfolio_container_header'>
+                <MacButtons />
+                <span className='portfolio_container_header_title'>_portfolio.app</span>
             </div>
-            ))}
+            <div className='portfolio_container_content'>
+                <div className="portfolio_container_content_languages">
+                    <div className='portfolio_container_content_languages_contenu'> 
+                        {Object.keys(portfolioData.languages).map((language) => (
+                            <div key={language} onClick={() => handleLanguageClick(language)}>
+                                {language}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className='portfolio_container_content_separateur'></div>
+                <div className="portfolio_container_content_projets">
+                {selectedLanguage && (
+                    <div className="portfolio_container_content_projets_contenu">
+                        {Object.keys(portfolioData.languages[selectedLanguage]).map((project) => (
+                            <div key={project} onClick={() => handleProjectClick(project)}>
+                            {project}
+                            </div>
+                        ))}
+                    </div>
+                )}
+                </div>
+                <div className='portfolio_container_content_separateur'></div>
+                <div className="portfolio_container_content_details">
+                {selectedProject && (
+                    <div className='portfolio_container_content_details_contenu'>
+                        <div>Title: {portfolioData.languages[selectedLanguage][selectedProject].title}</div>
+                            <div>
+                                Images:
+                                {portfolioData.languages[selectedLanguage][selectedProject].images.map((image, index) => (
+                                <img key={index} src={image} alt={`project-${selectedProject}-image-${index}`} />
+                                ))}
+                            </div>
+                        <div>Text: {portfolioData.languages[selectedLanguage][selectedProject].text}</div>
+                    </div>
+                )}
+                </div>
+            </div>
         </div>
-
-        {selectedLanguage && (
-            <div className="project-container">
-            {Object.keys(portfolioData.languages[selectedLanguage]).map((project) => (
-                <div key={project} onClick={() => handleProjectClick(project)}>
-                {project}
-                </div>
-            ))}
-            </div>
-        )}
-
-        {selectedProject && (
-            <div className="details-container">
-                <div>Title: {portfolioData.languages[selectedLanguage][selectedProject].title}</div>
-                <div>
-                    Images:
-                    {portfolioData.languages[selectedLanguage][selectedProject].images.map((image, index) => (
-                    <img key={index} src={image} alt={`project-${selectedProject}-image-${index}`} />
-                    ))}
-                </div>
-                <div>Text: {portfolioData.languages[selectedLanguage][selectedProject].text}</div>
-            </div>
-        )}
-
         <Cursor />
         </div>
     );
