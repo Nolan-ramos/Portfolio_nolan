a933460650231970b90b73921522c16f80d5e681
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-10-26 09:53:25 +0200

    portfolio

---
+++ src/components/portfolio/Close.jsx
@@ -0,0 +1,7 @@
+function Close(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="m7 7l10 10M7 17L17 7"></path></svg>
+    )
+}
+
+export default Close;
--- src/views/portfolio/Portfolio.jsx
+++ src/views/portfolio/Portfolio.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import Cursor from '../../components/cursor/Cursor';
 import MacButtons from '../../components/mac_buttons/mac_buttons';
+import Close from '../../components/portfolio/Close.jsx';
+import Folder from '../../components/portfolio/Folder.jsx';
+import Open from '../../components/portfolio/Open.jsx';
 import Title from '../../components/title/Title';
 import './portfolio.scss';
 import portfolioData from './portfolioData.json';
 
+// import Css from '../../components/portfolio/Css.jsx';
+// import Figma from '../../components/portfolio/Figma.jsx';
+// import Javascript from '../../components/portfolio/Javascript.jsx';
+// import Vuejs from '../../components/portfolio/Vuejs.jsx';
+
+import fake3 from './img/fake3.jpg';
+import fake4 from './img/fake4.jpg';
+import fakeSlide from './img/fake_slide.jpg';
+import fakeSlidee from './img/fake_slidee.jpg';
+
 function Portfolio() {
     const [selectedLanguage, setSelectedLanguage] = useState(null);
     const [selectedProject, setSelectedProject] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const handleLanguageClick = (language) => {
         setSelectedLanguage(language);
@@ -18,54 +33,96 @@ function Portfolio() {
         setSelectedProject(project);
     };
 
+    useEffect(() => {
+        if (selectedLanguage && selectedProject && portfolioData.languages[selectedLanguage] && portfolioData.languages[selectedLanguage][selectedProject]) {
+            const interval = setInterval(() => {
+                setCurrentIndex((prevIndex) =>
+                    prevIndex === portfolioData.languages[selectedLanguage][selectedProject].images.length - 1 ? 0 : prevIndex + 1
+                );
+            }, 2000);
+            return () => clearInterval(interval);
+        }
+    }, [selectedLanguage, selectedProject]);
+
     return (
         <div className='portfolio fade-in-component'>
-        <Title title="Portfolio" />
-        <div className='portfolio_container'>
-            <div className='portfolio_container_header'>
-                <MacButtons closeUrl="/"/>
-                <span className='portfolio_container_header_title'>_portfolio.app</span>
-            </div>
-            <div className='portfolio_container_content'>
-                <div className="portfolio_container_content_languages">
-                    <div>
-                        {Object.keys(portfolioData.languages).map((language) => (
-                            <div key={language} onClick={() => handleLanguageClick(language)}>
-                                {language}
-                            </div>
-                        ))}
-                    </div>
+            <Title title="Portfolio" />
+            <div className='portfolio_container'>
+                <div className='portfolio_container_header'>
+                    <MacButtons closeUrl='/'/>
+                    <span className='portfolio_container_header_title'>_portfolio.app</span>
                 </div>
-                <div className="portfolio_container_content_separateur"></div>
-                <div className="portfolio_container_content_projets">
-                {selectedLanguage && (
-                    <div className="portfolio_container_content_projets_contenu">
-                        {Object.keys(portfolioData.languages[selectedLanguage]).map((project) => (
-                            <div key={project} onClick={() => handleProjectClick(project)}>
-                            {project}
-                            </div>
-                        ))}
+                <div className='portfolio_container_content'>
+                    <div className='portfolio_container_content_languages'>
+                        <div className='portfolio_container_content_languages_contenu'>
+                            {Object.keys(portfolioData.languages).map((language) => (
+                                <div
+                                    key={language}
+                                    onClick={() => handleLanguageClick(language)}
+                                    className={`${selectedLanguage === language ? 'language_active cursor_change' : 'cursor_change'}`}
+                                    >
+                                    {selectedLanguage === language ? (
+                                        <span class="open"><Open /></span>
+                                    ) : (
+                                        <span class="close"><Close /></span>
+                                    )}
+                                    <p>{language}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                )}
-                </div>
-                <div className="portfolio_container_content_separateur"></div>
-                <div className="portfolio_container_content_details">
-                {selectedProject && (
-                    <div className='portfolio_container_content_details_contenu'>
-                        <div>Title: {portfolioData.languages[selectedLanguage][selectedProject].title}</div>
-                            <div>
-                                Images:
-                                {portfolioData.languages[selectedLanguage][selectedProject].images.map((image, index) => (
-                                <img key={index} src={image} alt={`project-${selectedProject}-image-${index}`} />
+                    <div className='portfolio_container_content_separateur separateur_resp'></div>
+                    <div className='portfolio_container_content_projets'>
+                        {selectedLanguage && (
+                            <div className='portfolio_container_content_projets_contenu'>
+                                {Object.keys(portfolioData.languages[selectedLanguage]).map((project) => (
+                                    <div
+                                        key={project}
+                                        onClick={() => handleProjectClick(project)}
+                                        className={selectedProject === project ? 'projet_active' : ''}
+                                    >
+                                        <Folder/>
+                                        {/* {selectedLanguage === "JavaScript" && <Javascript />}
+                                        {selectedLanguage === "Figma" && <Figma />}
+                                        {selectedLanguage === "VueJs" && <Vuejs />}
+                                        {selectedLanguage === "CSS" && <Css />} */}
+                                        {project}
+                                    </div>
+                                    
                                 ))}
                             </div>
-                        <div>Text: {portfolioData.languages[selectedLanguage][selectedProject].text}</div>
+                        )}
                     </div>
-                )}
+                    <div className='portfolio_container_content_separateur'></div>
+                    <div className='portfolio_container_content_details'>
+                        {selectedProject && (
+                            <div className='portfolio_container_content_details_contenu'>
+                                <div className='portfolio_container_content_details_contenu_slide'>
+                                    {portfolioData.languages[selectedLanguage] && portfolioData.languages[selectedLanguage][selectedProject] && (
+                                        <img
+                                            className={`portfolio_container_content_details_contenu_slide_image ${currentIndex === 0 ? 'slide-enter' : 'slide-exit'}`}
+                                            key={currentIndex}
+                                            src={
+                                                portfolioData.languages[selectedLanguage][selectedProject].images[currentIndex] === 'fake_slide.jpg'
+                                                    ? fakeSlide
+                                                    : portfolioData.languages[selectedLanguage][selectedProject].images[currentIndex] === 'fake_slidee.jpg'
+                                                    ? fakeSlidee
+                                                    : portfolioData.languages[selectedLanguage][selectedProject].images[currentIndex] === 'fake3.jpg'
+                                                    ? fake3
+                                                    : fake4
+                                            }
+                                            alt={`project-${selectedProject}-image-${currentIndex}`}
+                                        />
+                                    )}
+                                </div>
+                                <div>Text: {portfolioData.languages[selectedLanguage][selectedProject].text}</div>
+                                <div>Title: {portfolioData.languages[selectedLanguage][selectedProject].title}</div>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-        <Cursor />
+            <Cursor />
         </div>
     );
 }
